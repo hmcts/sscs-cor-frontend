9c04ae266143d70094863906be62c92e5adaca4d
David Thompson <davethompson21@gmail.com>
2018-07-31 13:49:34 +0100

    Fixing incorrect parts of terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,11 +6,11 @@ data "vault_generic_secret" "cookiesecret" {
   path = "secret/${var.infrastructure_env}/sscs/sscscorcookiesecret"
 }
 
-data "vault_generic_secret" "hpkp_tya_sha_1" {
+data "vault_generic_secret" "hpkp_cor_sha_1" {
   path = "secret/${var.infrastructure_env}/sscs/hpkp_cor_sha_1"
 }
 
-data "vault_generic_secret" "hpkp_tya_sha_2" {
+data "vault_generic_secret" "hpkp_cor_sha_2" {
   path = "secret/${var.infrastructure_env}/sscs/hpkp_cor_sha_2"
 }
 
@@ -21,7 +21,7 @@ locals {
   ApiUrl = "${var.env == "preview" ? "http://sscs-cor-api-aat.service.core-compute-aat.internal" : local.localApiUrl}"
 }
 
-module "tya-frontend" {
+module "sscs-cor-frontend" {
   source               = "git@github.com:contino/moj-module-webapp?ref=master"
   product              = "${var.product}-${var.component}"
   location             = "${var.location}"
@@ -38,8 +38,8 @@ module "tya-frontend" {
     WEBSITE_NODE_DEFAULT_VERSION = "8.11.3"
     NODE_ENV                     = "${var.infrastructure_env}"
     COOKIE_SECRET                = "${data.vault_generic_secret.cookiesecret.data["value"]}"
-    HPKP_SHA256                  = "${data.vault_generic_secret.hpkp_tya_sha_1.data["value"]}"
-    HPKP_SHA256_BACKUP           = "${data.vault_generic_secret.hpkp_tya_sha_2.data["value"]}"
+    HPKP_SHA256                  = "${data.vault_generic_secret.hpkp_cor_sha_1.data["value"]}"
+    HPKP_SHA256_BACKUP           = "${data.vault_generic_secret.hpkp_cor_sha_2.data["value"]}"
   }
 }
 
