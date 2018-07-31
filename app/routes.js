e2231eb45609a77385ac6d74ead0fb783101f78d
David Thompson <davethompson21@gmail.com>
2018-07-31 11:55:22 +0100

    eslint ignore public and govuk_modules

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const paths = require('paths');
 
+/* eslint-disable new-cap */
 const router = express.Router();
+/* eslint-enable new-cap */
 
 router.get(paths.helloWorld, (req, res) => {
   res.render('hello-world');
