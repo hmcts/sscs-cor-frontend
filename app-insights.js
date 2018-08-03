f421cc4e59d3d77c858128947072df48afa89094
Dave Thompson <davethompson21@gmail.com>
2018-08-03 11:52:54 +0100

    [PREVIEW] Adding application insights integration (#8)
    * Adding application insights integration
    
    * Application insights does not play nice with Puppeteer
    
    * adding option to disable it when starting the app
    * applying this option for testing

---
+++ app-insights.js
@@ -0,0 +1,17 @@
+const applicationInsights = require('applicationinsights');
+const config = require('config');
+
+const enable = () => {
+  const iKey = config.get('appInsights.instrumentationKey');
+  applicationInsights.setup(iKey).setAutoCollectConsole(true, true)
+    .start();
+};
+
+const trackException = exception => {
+  applicationInsights.defaultClient.trackException({ exception });
+};
+
+module.exports = {
+  enable,
+  trackException
+};
