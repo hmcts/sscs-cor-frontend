d1da72aaed057e988476cc16197108fc82de1972
David Thompson <davethompson21@gmail.com>
2018-08-03 13:56:23 +0100

    Adding error-handler middleware

---
+++ app/middleware/error-handler.js
@@ -0,0 +1,19 @@
+const { Logger } = require('@hmcts/nodejs-logging');
+const { INTERNAL_SERVER_ERROR, NOT_FOUND } = require('http-status-codes');
+
+const logger = Logger.getLogger('error-handler.js');
+
+function pageNotFoundHandler(req, res) {
+  res.status(NOT_FOUND);
+  res.render('errors/404.html');
+}
+
+/* eslint-disable no-unused-vars */
+function coreErrorHandler(error, req, res, next) {
+  logger.error(`500 Error from request ${req.originalUrl} : ${JSON.stringify(error)} : ${error}`);
+  res.status(INTERNAL_SERVER_ERROR);
+  res.render('errors/500.html');
+}
+/* eslint-enable no-unused-vars */
+
+module.exports = { pageNotFoundHandler, coreErrorHandler };
