c536151372a280c56378b45e5d32ee41d5c78bd8
David Thompson <davethompson21@gmail.com>
2018-08-06 16:11:06 +0100

    Adding redis module to infrastructure terraform
    * allowing config to be overridden by env vars

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -21,7 +21,16 @@ module "sscs-cor-frontend" {
     SSCS_API_URL                 = "${local.ApiUrl}"
     WEBSITE_NODE_DEFAULT_VERSION = "8.11.1"
     NODE_ENV                     = "${var.infrastructure_env}"
+    REDIS_URL                    = "redis://ignore:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
+    SESSION_SECRET               = "${module.redis-cache.access_key}"
   }
 }
 
-
+module "redis-cache" {
+  source      = "git@github.com:contino/moj-module-redis?ref=master"
+  product     = "${var.product}-redis"
+  location    = "${var.location}"
+  env         = "${var.env}"
+  subnetid    = "${data.terraform_remote_state.core_apps_infrastructure.subnet_ids[1]}"
+  common_tags = "${var.common_tags}"
+}
