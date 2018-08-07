d1c5d149bffad97b949c3d313025b32782850716
bendiggle <ben.diggle5@gmail.com>
2018-08-07 16:46:49 +0100

    change mock data question

--- test/mock/question.js
+++ test/mock/question.js
@@ -2,6 +2,6 @@ module.exports = {
   path: '/continuous-online-hearings/:onlineHearingId/questions/:questionId',
   template: {
     question_id: params => `${params.questionId}`,
-    question_header_text: 'Hey friend!'
+    question_header_text: 'What is the meaning of life?'
   }
 };
