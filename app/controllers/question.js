444223d3587ec48d0df1879f411ac40c220b735d
bendiggle <ben.diggle5@gmail.com>
2018-08-07 16:05:59 +0100

    add unit tests to controller

--- app/controllers/question.js
+++ app/controllers/question.js
@@ -1,10 +1,33 @@
 const appInsights = require('app-insights');
 const express = require('express');
-const getQuestion = require('app/middleware/getQuestion');
 
-// eslint-disable-next-line new-cap
-const router = express.Router();
+function getQuestion(getQuestionService) {
+  return (req, res, next) => {
+    const hearingId = req.params.hearingId;
+    const questionId = req.params.questionId;
+    return getQuestionService(hearingId, questionId)
+      .then(response => {
+        res.render('question.html', {
+          question: {
+            header: response.body.question_header_text
+          }
+        });
+      })
+      .catch(error => {
+        appInsights.trackException(error);
+        next(error);
+      });
+  };
+}
 
-router.get('/:hearingId/:questionId', getQuestion);
+function setupQuestionController(deps) {
+  // eslint-disable-next-line new-cap
+  const router = express.Router();
+  router.get('/:hearingId/:questionId', getQuestion(deps.getQuestionService));
+  return router;
+}
 
-module.exports = router;
+module.exports = {
+  setupQuestionController,
+  getQuestion
+};
--- test/unit/controllers/question.test.js
+++ test/unit/controllers/question.test.js
@@ -1,24 +1,88 @@
 const { expect, sinon } = require('test/chai-sinon');
-const question = require('app/controllers/question');
-const nock = require('nock');
-const config = require('config');
-
-const apiUrl = config.get('api.url');
+const { getQuestion, setupQuestionController } = require('app/controllers/question');
+const { INTERNAL_SERVER_ERROR } = require('http-status-codes');
+const appInsights = require('app-insights');
+const express = require('express');
 
 describe('question.js', () => {
-  const req = sinon.stub();
-  const res = sinon.stub();
   const next = sinon.stub();
+  const req = {}, res = {};
 
-  const apiResponse = nock
+  req.params = {
+    hearingId: '1',
+    questionId: '2'
+  };
 
-  it('should call render with the question header', () => {
-      nock(apiUrl)
-          .get(`/continuous-online-hearings/${hearingId}/questions/${questionId}`)
-          .reply(HttpStatus.OK, { appealId });
-    question(req, res, next)
-      .then(response => {
+  res.render = sinon.stub();
 
-      });
+  describe('getQuestion', () => {
+    // eslint-disable-next-line init-declarations
+    let questionService;
+
+    beforeEach(() => {
+      questionService = null;
+      sinon.stub(appInsights, 'trackException');
+    });
+
+    afterEach(() => {
+      appInsights.trackException.restore();
+    });
+
+    it('should call render with the template and question header', () => {
+      const questionHeading = 'What is the meaning of life?';
+      questionService = () => (
+        new Promise(resolve => {
+          resolve({
+            body: {
+              question_header_text: questionHeading
+            }
+          });
+        })
+      );
+      const getQuestionMiddleware = getQuestion(questionService);
+      return getQuestionMiddleware(req, res, next)
+        .then(() => {
+          expect(res.render).to.have.been.calledWith('question.html', {
+            question: {
+              header: questionHeading
+            }
+          });
+        });
+    });
+
+    it('should call next and appInsights with the error when there is one', () => {
+      const error = { value: INTERNAL_SERVER_ERROR, reason: 'Server Error' };
+      questionService = () => (
+        new Promise((resolve, reject) => {
+          reject(error);
+        })
+      );
+      const getQuestionMiddleware = getQuestion(questionService);
+      return getQuestionMiddleware(req, res, next)
+        .then(() => {
+          expect(appInsights.trackException).to.have.been.calledOnce.calledWith(error);
+          expect(next).to.have.been.calledWith(error);
+        });
+    });
+  });
+
+  describe('setupQuestionController', () => {
+    beforeEach(() => {
+      sinon.stub(express, 'Router').returns({ get: sinon.stub() });
+    });
+
+    afterEach(() => {
+      express.Router.restore();
+    });
+
+    it('calls router.get with the path and middleware', () => {
+      setupQuestionController({ getQuestionService: {} });
+      expect(express.Router().get).to.have.been.calledWith('/:hearingId/:questionId');
+    });
+
+    it('returns the router', () => {
+      const controller = setupQuestionController({ getQuestionService: {} });
+      expect(controller).to.equal(express.Router());
+    });
   });
 });
