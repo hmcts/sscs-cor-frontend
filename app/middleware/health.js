6502a531d50db4941c8472d8e40c96596dd5e922
David Thompson <davethompson21@gmail.com>
2018-08-09 20:13:56 +0100

    Configuring connection to backend
    * adding api status to health check

--- app/middleware/health.js
+++ app/middleware/health.js
@@ -1,7 +1,26 @@
+const request = require('superagent');
+const { OK } = require('http-status-codes');
+const appInsights = require('app-insights');
+const { Logger } = require('@hmcts/nodejs-logging');
 const healthCheck = require('@hmcts/nodejs-healthcheck');
 
-module.exports = (req, res) => {
+const logger = Logger.getLogger('health.js');
+const apiUrl = require('config').get('api.url');
+
+async function getApiHealth() {
+  try {
+    const result = await request.get(`${apiUrl}/health`);
+    return result.status === OK ? 'UP' : 'DOWN';
+  } catch (error) {
+    appInsights.trackException(error);
+    logger.error('Error trying to check health of API', error);
+    return 'DOWN';
+  }
+}
+
+module.exports = async(req, res) => {
   const redisStatus = req.session ? 'UP' : 'DOWN';
-  const status = Object.assign({}, healthCheck.up(), { redisStatus });
+  const apiStatus = await getApiHealth();
+  const status = { ...healthCheck.up(), ...{ redisStatus, apiStatus } };
   res.json(status);
 };
