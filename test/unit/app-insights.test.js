fdd2a4cc642ced5b3ba64fd2c7b340fb4caf76d7
bendiggle <ben.diggle5@gmail.com>
2018-08-09 12:53:14 +0100

    trying to get tests to pass

--- test/unit/app-insights.test.js
+++ test/unit/app-insights.test.js
@@ -3,37 +3,69 @@ const applicationInsights = require('applicationinsights');
 const { enable, trackException } = require('app-insights');
 const config = require('config');
 
-describe.only('app-insights.js', () => {
-  describe('trackException', () => {
-    beforeEach(() => {
-      applicationInsights.defaultClient = {
-        trackException: sinon.stub()
-      };
-    });
+describe('app-insights.js', () => {
 
-    it('should call trackException with the exception passed', () => {
-      const exception = 'Exception Error';
-      trackException(exception);
-      expect(applicationInsights.defaultClient.trackException).to.have.been.calledWith({
-        exception
-      });
-    });
-  });
+
+
   describe('enable', () => {
+
+    const sb = sinon.sandbox.create();
+
+    after(() => {
+      sb.restore();
+    })
+
+
     let startStub = null;
     beforeEach(() => {
-      sinon.spy(config, 'get');
-      startStub = sinon.stub();
-      sinon.stub(applicationInsights, 'setup').withArgs('iKey')
+      sb.spy(config, 'get');
+      startStub = sb.stub();
+      sb.stub(applicationInsights, 'setup').withArgs('iKey')
         .returns({
-          setAutoCollectConsole: sinon.stub().withArgs(true, true)
+          setAutoCollectConsole: sb.stub().withArgs(true, true)
             .returns({ start: startStub })
         });
     });
 
+    afterEach(() => {
+      // config.get.restore();
+      // applicationInsights.setup.restore();
+      // applicationInsights.setup('iKey').setAutoCollectConsole.restore();
+      // sb.restore();
+    })
+
     it('should call start', () => {
       enable();
       expect(startStub).to.have.been.called;
+    });
+  });
+  describe('trackException', () => {
+
+    const sb = sinon.sandbox.create();
+
+    after(() => {
+      sb.restore();
+    })
+
+    let trackStub;
+    before(() => {
+      enable();
+    });
+    beforeEach(() => {
+      sb.stub(applicationInsights.defaultClient, 'trackException');
+      // trackStub = sinon.stub(applicationInsights).returns({
+      //   defaultClient: {
+      //     trackException: sinon.stub()
+      //   }
+      // })
+    });
+    // afterEach(() => {
+    //   trackStub.restore();
+    // });
+    it('should call trackException with the exception passed', () => {
+      const exception = 'Exception Error';
+      trackException(exception);
+      expect(applicationInsights.defaultClient.trackException).to.have.been.calledWith({ exception });
     });
   });
 });
