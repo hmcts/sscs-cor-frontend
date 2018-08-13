0f23bc2fcdc09623290978539ded956a3c4b0245
David Thompson <davethompson21@gmail.com>
2018-08-13 14:39:02 +0100

    Using white-space pre-wrap to retain line breaks in question body
    * assuming that line-breaks are returned from the API as `\n`
    * adding line breaks to mock data that is also used for creating a real question in functional testing
    * applying two-thirds grid layout to sending evidence content

--- test/mock/data/question.js
+++ test/mock/data/question.js
@@ -1,10 +1,9 @@
+/* eslint-disable max-len */
 module.exports = {
   path: '/continuous-online-hearings/:onlineHearingId/questions/:questionId',
   template: {
     question_id: params => `${params.questionId}`,
     question_header_text: 'How do you interact with people?',
-    question_body_text: `You said you avoid interacting with people if possible. We’d like to know 
-      more about the times when you see friends and family. Tell us about three separate occasions 
-      in 2017 that you have met with friends and family.`
+    question_body_text: 'You said you avoid interacting with people if possible. We\'d like to know more about the times when you see friends and family.\n\nTell us about three separate occasions in 2017 that you have met with friends and family.\n\nTell us:\n\n- who you met\n\n- when\n\n- where\n\n- how it made you feel'
   }
 };
