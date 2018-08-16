ddea763c7bbafc74b67075d5339c9eb17f24349f
bendiggle <ben.diggle5@gmail.com>
2018-08-16 15:15:24 +0100

    modify endpoint uri

--- app/services/postAnswer.js
+++ app/services/postAnswer.js
@@ -6,7 +6,7 @@ const apiUrl = config.get('api.url');
 async function postAnswer(hearingId, questionId, answerState, answerText) {
   try {
     const response = await request
-      .put(`${apiUrl}/continuous-online-hearings/${hearingId}/questions/${questionId}/answers`)
+      .put(`${apiUrl}/continuous-online-hearings/${hearingId}/questions/${questionId}`)
       .send({
         answer_state: answerState,
         answer: answerText
