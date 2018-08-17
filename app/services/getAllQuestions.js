763814e0a33f7c5cfd901221362b4c747519c221
bendiggle <ben.diggle5@gmail.com>
2018-08-17 11:38:45 +0100

    modify api endpoint for getting list of questions

--- app/services/getAllQuestions.js
+++ app/services/getAllQuestions.js
@@ -6,7 +6,7 @@ const apiUrl = config.get('api.url');
 async function getQuestion(hearingId) {
   try {
     const response = await request
-      .get(`${apiUrl}/continuous-online-hearings/${hearingId}/questions`);
+      .get(`${apiUrl}/continuous-online-hearings/${hearingId}`);
     return Promise.resolve(response.body);
   } catch (error) {
     return Promise.reject(error);
