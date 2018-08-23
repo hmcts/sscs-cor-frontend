57731638dfe859c36b720a22e5c3b81337c9328e
Jozz Hart <me@jozzhart.com>
2018-08-23 15:56:29 +0100

    convert client-side to use typescript

--- app/assets/javascript/main.js
+++
@@ -1,8 +0,0 @@
-const $ = require('jquery');
-const GOVUKFrontend = require('govuk-frontend');
-const expandingTextbox = require('./expanding-textbox');
-
-$(document).ready(() => {
-  GOVUKFrontend.initAll();
-  expandingTextbox.init();
-});
