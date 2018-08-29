dbfa26d46b73631d27f21437fc67c07406be4a78
Jozz Hart <me@jozzhart.com>
2018-08-29 16:37:05 +0100

    minor syntax clean

--- test/page-objects/submit_question.js
+++ test/page-objects/submit_question.js
@@ -4,7 +4,6 @@ const BasePage = require('test/page-objects/base');
 class SubmitQuestionPage extends BasePage {
   constructor(page, hearingId, questionId) {
     super(page);
-    this.page = page;
     this.pagePath = `${question}/${hearingId}/${questionId}/submit`;
   }
 
--- test/page-objects/task-list.js
+++ test/page-objects/task-list.js
@@ -4,7 +4,6 @@ const BasePage = require('test/page-objects/base');
 class TaskListPage extends BasePage {
   constructor(page, hearingId) {
     super(page);
-    this.page = page;
     let path = taskList;
     if (hearingId) {
       path += `/${hearingId}`;
