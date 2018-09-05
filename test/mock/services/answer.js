d96221e66360f8ab7593dc0a57f4e15670db2469
David Thompson <davethompson21@gmail.com>
2018-09-05 10:33:44 +0100

    More functional testing and enforcing "login"
    * adding simple state to mock backend e.g. keeping track of question answer state
    * no longer cache any dyson stubs as they are not deterministic anymore
    * this is to facilitate more complex functional testing
    * protecting all routes behind "auth", from now you must have a valid case in CCD and "login" using the associated email address
    * removing use of hearing ID in task list URL, it is held in the redis session after successfully signing in with an email address
    * adding extra tests now possible due to having simple state in the stub backend

--- test/mock/services/answer.js
+++ test/mock/services/answer.js
@@ -1,9 +1,17 @@
 const { NO_CONTENT } = require('http-status-codes');
+const cache = require('memory-cache');
+
+function cacheAnswerState(questionId, answerText) {
+  cache.put(`${questionId}.state`, 'draft');
+  cache.put(`${questionId}.answer`, answerText);
+}
 
 module.exports = {
   path: '/continuous-online-hearings/:hearingId/questions/:questionId',
   method: 'PUT',
+  cache: false,
   status: (req, res, next) => {
+    cacheAnswerState(req.params.questionId, req.body.answer);
     res.status(NO_CONTENT);
     next();
   }
