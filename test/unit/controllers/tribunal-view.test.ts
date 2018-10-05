fda0b760b79e239006813cf5b9ed955da098dac6
David Thompson <davethompson21@gmail.com>
2018-10-05 17:16:52 +0100

    Handling post of tribunal view page
    * adding validation with message to ensure that an option is selected

--- test/unit/controllers/tribunal-view.test.ts
+++ test/unit/controllers/tribunal-view.test.ts
@@ -1,7 +1,8 @@
 import { OnlineHearing } from 'app/server/services/getOnlineHearing'
 const { expect, sinon } = require('test/chai-sinon');
-const { getTribunalView, setupTribunalViewController } = require('app/server/controllers/tribunal-view');
+const { getTribunalView, postTribunalView, setupTribunalViewController } = require('app/server/controllers/tribunal-view');
 const express = require('express');
+const i18n = require('locale/en.json');
 import * as Paths from 'app/server/paths';
 import * as moment from 'moment';
 
@@ -9,6 +10,7 @@ describe('controllers/tribunal-view', () => {
   let req: any;
   let res: any;
   let hearingDetails: OnlineHearing;
+  let respondBy;
 
   beforeEach(() => {
     hearingDetails = {
@@ -27,18 +29,21 @@ describe('controllers/tribunal-view', () => {
     req = {
       session: {
         hearing: hearingDetails
+      },
+      body: {
+        'accept-view': 'yes'
       }
     } as any;
     res = {
       render: sinon.spy(),
       redirect: sinon.spy()
     } as any;
+    respondBy = moment.utc(req.session.hearing.decision.decision_state_datetime).add(7, 'day').format();
   });
 
   describe('getTribunalView', () => {
     it('renders tribunal view page with issued decision', async() => {
       await getTribunalView(req, res);
-      const respondBy = moment.utc(req.session.hearing.decision.decision_state_datetime).add(7, 'day').format();
       expect(res.render).to.have.been.calledOnce.calledWith('tribunal-view.html', { decision: hearingDetails.decision, respondBy });
     });
 
@@ -55,12 +60,45 @@ describe('controllers/tribunal-view', () => {
     });
   });
 
+  describe('postTribunalView', () => {
+    describe('validation failed', () => {
+      beforeEach(() => {
+        req.body['accept-view'] = '';
+        postTribunalView()(req, res);
+      });
+
+      it('renders the view with the error message', () => {
+        expect(res.render).to.have.been.calledOnce.calledWith('tribunal-view.html', {
+          decision: hearingDetails.decision,
+          respondBy,
+          error: i18n.tribunalView.error.empty
+        });
+      });
+    });
+    describe('validation passed', () => {
+      it('sets flag in session if accepts is yes', () => {
+        postTribunalView()(req, res);
+        expect(req.session).to.have.property('tribunalViewAcceptedThisSession', true);
+      });
+      it('redirects to view accepted page if accepts is yes', () => {
+        postTribunalView()(req, res);
+        expect(res.redirect).to.have.been.calledOnce.calledWith(Paths.tribunalViewAccepted);
+      });
+      it('redirects to hearing confirm page if accepts is no', () => {
+        req.body['accept-view'] = 'no';
+        postTribunalView()(req, res);
+        expect(res.redirect).to.have.been.calledOnce.calledWith(Paths.hearingConfirm);
+      });
+    });
+  });
+
   describe('setupTribunalViewController', () => {
     let deps;
     beforeEach(() => {
       deps = {};
       sinon.stub(express, 'Router').returns({
-        get: sinon.stub()
+        get: sinon.spy(),
+        post: sinon.spy()
       });
     });
 
@@ -72,6 +110,12 @@ describe('controllers/tribunal-view', () => {
       setupTribunalViewController(deps);
       // eslint-disable-next-line new-cap
       expect(express.Router().get).to.have.been.calledWith(Paths.tribunalView);
+    });
+
+    it('calls router.post with the path and middleware', () => {
+      setupTribunalViewController(deps);
+      // eslint-disable-next-line new-cap
+      expect(express.Router().post).to.have.been.calledWith(Paths.tribunalView);
     });
 
     it('returns the router', () => {
