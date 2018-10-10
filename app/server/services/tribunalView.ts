38432b0dffd221c768191531cfabde522cffe67c
David Thompson <davethompson21@gmail.com>
2018-10-10 14:05:39 +0100

    Call backend service to record acceptance of tribunal's view

---
+++ app/server/services/tribunalView.ts
@@ -0,0 +1,18 @@
+import * as request from 'superagent';
+const config = require('config');
+const apiUrl = config.get('api.url');
+
+async function recordTribunalViewResponse(hearingId: string, reply: string, reason?: string) {
+  try {
+    const response: request.Response = await request
+      .patch(`${apiUrl}/continuous-online-hearings/${hearingId}/tribunal-view`)
+      .send({ reply, reason });
+    return Promise.resolve();
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+export {
+  recordTribunalViewResponse
+};
