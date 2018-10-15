09cd44ddb02498219f742f4159ec747d86e18249
Jozz Hart <me@jozzhart.com>
2018-10-15 13:19:14 +0100

    Remove jQuery from ExpandingTextbox
    Add domready and computed-styles libraries

--- test/unit/client/javascript/expanding-textbox.test.ts
+++ test/unit/client/javascript/expanding-textbox.test.ts
@@ -1,16 +1,14 @@
 import { expect } from 'test/chai-sinon';
-import { ExpandingTextBox } from 'app/client/javascript/expanding-textbox';
+import * as expandingTextBox from 'app/client/javascript/expanding-textbox';
 
-document.body.innerHTML = `<textarea class="auto-expand" style="border-top: 2px; border-bottom: 2px;"></textarea>`;
+document.body.innerHTML = '<textarea class="auto-expand" style="border-top: 2px; border-bottom: 2px;"></textarea>';
 
 describe('expanding-textbox', () => {
   describe('#autoExpand', () => {
     it('sets height to scroll height + border widths', () => {
-      const expandingTextBox = new ExpandingTextBox();
       const target = document.querySelector<HTMLInputElement>('textarea.auto-expand');
-      
       expect(target.style).to.have.property('height', '');
-      expandingTextBox.autoExpand({target});
+      expandingTextBox.autoExpand({ target });
       expect(target.style).to.have.property('height', '4px');
     });
   });
