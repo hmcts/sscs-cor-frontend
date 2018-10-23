0ecff1639e0bbfaab51d51b14dc1e7f05bab5320
David Thompson <davethompson21@gmail.com>
2018-10-23 20:47:53 +0100

    Adding upload-evidence page
    * this is just a template, no upload processing is taking place yet, coming next
    * requests will return 404 response if feature is not enabled or overridden
    * some minor tidying also

--- test/page-objects/hearing-confirm.ts
+++ test/page-objects/hearing-confirm.ts
@@ -18,7 +18,7 @@ export class HearingConfirmPage extends BasePage {
   async submit() {
     await Promise.all([
       this.page.waitForNavigation(),
-      await this.clickElement('#submit-button button')
+      this.clickElement('#submit-button button')
     ]);
   }
 }
