958be63f57d033dcb10f368e754c93ce0cc177d9
David Thompson <davethompson21@gmail.com>
2018-10-25 20:27:23 +0100

    Refactor evidence service
    * it's now a class
    * renaming unit test file
    * more unit tests

--- app/server/services/evidence.ts
+++ app/server/services/evidence.ts
@@ -1,36 +1,43 @@
-const config = require('config');
-const request = require('request-promise');
+import * as request from 'request-promise';
 
-const apiUrl = config.get('api.url');
+export class EvidenceService {
+  private apiUrl: string;
 
-export async function upload(hearingId, questionId, file) {
-  try {
-    const body = await request.post({
-      url: `${apiUrl}/continuous-online-hearings/${hearingId}/questions/${questionId}/evidence`,
-      formData: {
-        file: {
-          value: file.buffer,
-          options: {
-            filename: file.originalname,
-            contentType: file.mimetype
+  constructor(apiUrl: string) {
+    this.apiUrl = apiUrl;
+  }
+
+  async upload(hearingId: string, questionId: string, file) {
+    try {
+      const body = await request.post({
+        url: `${this.apiUrl}/continuous-online-hearings/${hearingId}/questions/${questionId}/evidence`,
+        formData: {
+          file: {
+            value: file.buffer,
+            options: {
+              filename: file.originalname,
+              contentType: file.mimetype
+            }
           }
         }
-      }
-    });
-    return Promise.resolve(body);
-  } catch (error) {
-    return Promise.reject(error);
+      });
+      return Promise.resolve(body);
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
-}
 
-export async function remove(hearingId, questionId, fileId) {
-  try {
-    await request
-      .delete(`${apiUrl}/continuous-online-hearings/${hearingId}/questions/${questionId}/evidence/${fileId}`)
-      .set('Content-Length', '0')
-      .send();
-    return Promise.resolve();
-  } catch (error) {
-    return Promise.reject(error);
+  async remove(hearingId: string, questionId: string, fileId: string) {
+    try {
+      await request.delete({
+        url: `${this.apiUrl}/continuous-online-hearings/${hearingId}/questions/${questionId}/evidence/${fileId}`,
+        headers: {
+          'Content-Length': '0'
+        }
+      });
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
 }
