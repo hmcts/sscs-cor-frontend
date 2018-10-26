41e8d404232188074aa6ba920ea14482c0293ab8
David Thompson <davethompson21@gmail.com>
2018-10-26 16:02:57 +0100

    Refactoring extend-deadline service
    * moving into hearing service class
    * using request-promise instead of superagent

--- app/server/services/hearing.ts
+++ app/server/services/hearing.ts
@@ -18,6 +18,10 @@ export interface OnlineHearing {
   decision?: OnlineHearingDecision;
 }
 
+interface ExtendDeadlineResponse {
+  deadline_expiry_date: string;
+}
+
 export class HearingService {
   private apiUrl: string;
 
@@ -27,7 +31,7 @@ export class HearingService {
 
   async getOnlineHearing(email): Promise<request.Response> {
     try {
-      const response = await request.get({
+      const response: request.Response = await request.get({
         uri: `${this.apiUrl}/continuous-online-hearings`,
         qs: { email },
         resolveWithFullResponse: true,
@@ -35,6 +39,18 @@ export class HearingService {
         json: true
       });
       return Promise.resolve(response);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  async extendDeadline(hearingId: string): Promise<ExtendDeadlineResponse> {
+    try {
+      const body = await request.patch({
+        uri: `${this.apiUrl}/continuous-online-hearings/${hearingId}`,
+        json: true
+      });
+      return Promise.resolve(body);
     } catch (error) {
       return Promise.reject(error);
     }
