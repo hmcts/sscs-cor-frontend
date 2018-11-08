e1f6bc01826d19432cb10e69b313c18e42c9bba5
Rob Doherty <robdohertykelly@gmail.com>
2018-11-08 17:10:10 +0000

    SSCS-4139: correct button name for crossbrowser tests

--- test/e2e/page-objects/cor/task-list.js
+++ test/e2e/page-objects/cor/task-list.js
@@ -7,7 +7,7 @@ function answerQuestion(question, answer) {
   I.click(question);
   I.waitForText(question);
   I.fillField('#question-field', answer);
-  // I.click('Submit');   // TODO: this can be un-commented once unique users can be setup
+  // I.click('Submit answer to the tribunal');   // TODO: this can be un-commented once unique users can be setup
 }
 
 module.exports = { answerQuestion };
