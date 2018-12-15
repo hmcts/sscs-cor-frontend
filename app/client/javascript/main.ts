995976b95368ee9eea41514b83cef0b34de3a0ab
Izel Aciman <izelaciman@gmail.com>
2018-12-15 03:13:10 +0000

    Add cookie banner and cookie enabled tests.

--- app/client/javascript/main.ts
+++ app/client/javascript/main.ts
@@ -1,9 +1,11 @@
 import * as govUK from 'govuk-frontend';
 import * as expandingTextBox from './expanding-textbox';
 import { EvidenceUpload } from './evidence-upload';
+import { CheckCookies } from './check-cookies';
 const domready = require('domready');
 
 let evidenceUpload;
+let checkCookies;
 
 function initEvidenceUpload() {
   const evidenceUploadContainer = document.getElementById('evidence-upload');
@@ -12,7 +14,14 @@ function initEvidenceUpload() {
   }
 }
 
+function initCheckCookies() {
+  checkCookies = new CheckCookies();
+  let isCookiesEnabled = checkCookies.testCookies(window);
+  checkCookies.toggleBanner(isCookiesEnabled);
+}
+
 domready(function () {
+  initCheckCookies();
   govUK.initAll();
   expandingTextBox.init();
   initEvidenceUpload();
