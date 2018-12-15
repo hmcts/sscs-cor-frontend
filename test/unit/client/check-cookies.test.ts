5fe5cd8d1d2917bfe9839e93229646cedf2425bd
Izel Aciman <izel.aciman@hmtcs.net>
2018-12-15 03:13:10 +0000

    Add cookie banner and cookie enabled tests.

---
+++ test/unit/client/check-cookies.test.ts
@@ -0,0 +1,38 @@
+import { expect } from 'test/chai-sinon';
+import { CheckCookies } from 'app/client/javascript/check-cookies';
+
+describe('expanding-textbox', () => {
+  let checkCookies;
+  before(() => {
+    checkCookies = new CheckCookies();
+    document.body.innerHTML = '<div id="app-cookie-banner"></div>';
+  });
+
+  describe('#cookieBanner', () => {
+    it('Cookie disabled', () => {
+      const mockWindow = { navigator : { cookieEnabled : false }, document : {} };
+      // Lock cookie property to simulate browser disabled cookies.
+      Object.defineProperty(mockWindow.document, 'cookie', {
+        value: '',
+        writable: false
+      });
+      let result = checkCookies.testCookies(mockWindow);
+      expect(result).to.equal(false);
+    });
+
+    it('Cookie enabled', () => {
+      const mockWindow = { navigator : { cookieEnabled : true }, document : {} };
+      let result = checkCookies.testCookies(mockWindow);
+      expect(result).to.equal(true);
+    });
+
+    it('Cookie banner toggle', () => {
+      const target: HTMLElement = document.getElementById(checkCookies.COOKIE_BANNER);
+      checkCookies.toggleBanner(true);
+      expect(target.style.display).to.equal('none');
+
+      checkCookies.toggleBanner(false);
+      expect(target.style.display).to.equal('block');
+    });
+  });
+});
