de0f4aac5095de32184fcb49c039b2eaf5c4c3a6
Izel Aciman <izelaciman@gmail.com>
2018-12-17 16:40:48 +0000

    [SSCS-4817] Cookie policy page init.

---
+++ test/unit/controllers/policies.test.ts
@@ -0,0 +1,45 @@
+
+const { expect, sinon } = require('test/chai-sinon');
+const { setupCookiePrivacyController, getCookiePrivacy } = require('app/server/controllers/policies.ts');
+const express = require('express');
+import * as Paths from 'app/server/paths';
+
+describe('controllers/policies.js', () => {
+  let req: any;
+  let res: any;
+
+  beforeEach(() => {
+    req = {
+      session: {}
+    } as any;
+    res = {
+      render: sinon.spy(),
+      redirect: sinon.spy()
+    } as any;
+  });
+
+  describe('getCookiePrivacy', () => {
+    it('renders Cookie Policy page', async() => {
+      await getCookiePrivacy(req, res);
+      expect(res.render).to.have.been.calledOnce.calledWith('policy-pages/cookie-privacy.html');
+    });
+  });
+
+  describe('setupCookiePrivacyController', () => {
+    beforeEach(() => {
+      sinon.stub(express, 'Router').returns({
+        get: sinon.stub()
+      });
+    });
+
+    afterEach(() => {
+      express.Router.restore();
+    });
+
+    it('calls router.get with the path and middleware', () => {
+      setupCookiePrivacyController();
+      // eslint-disable-next-line new-cap
+      expect(express.Router().get).to.have.been.calledWith(Paths.cookiePrivacy);
+    });
+  });
+});
