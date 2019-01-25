5fc9945ec624600b2f6838f10ce2af47e8465033
Chris Grimble <chris@cg-it-solutions.com>
2019-01-25 10:04:47 +0000

    Create and delete tidam users for each test. This logic was there but had been commented out since we turned off sIdam.

--- test/browser/bootstrap.ts
+++ test/browser/bootstrap.ts
@@ -72,7 +72,7 @@ async function bootstrapCcdCase() {
 
 async function bootstrapSidamUser(ccdCase) {
   try {
-    await sidam.registerRedirectUri();
+    // await sidam.registerRedirectUri();
     return await sidam.createUser(ccdCase);
   } catch (error) {
     console.log('Error bootstrapping SIDAM user', error);
@@ -83,11 +83,9 @@ async function bootstrapSidamUser(ccdCase) {
 export async function bootstrap() {
   try {
     const ccdCase = await bootstrapCcdCase();
-    // reinstate once we are able to use a stable deployment of SIDAM in AAT
-    // const sidamUser = await bootstrapSidamUser(ccdCase);
+    const sidamUser = await bootstrapSidamUser(ccdCase);
     const cohTestData = await bootstrapCoh(ccdCase);
-    // reinstate return of `sidamUser` once we are able to use a stable deployment of SIDAM in AAT
-    return { ccdCase, cohTestData };
+    return { ccdCase, cohTestData, sidamUser };
   } catch (error) {
     return Promise.reject(error);
   }
