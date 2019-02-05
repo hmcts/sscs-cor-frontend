3ffdb44be80d48e6bef1798f8e465ea2f2f65643
Rob Doherty <robdohertykelly@gmail.com>
2019-02-05 18:10:18 +0000

    setup crossbrowser testing, and added a simple test that answers all appellant questions

--- test/e2e/journeys/basic.test.js
+++
@@ -1,6 +0,0 @@
-Feature('Basic user journey');
-
-Scenario('A PIP appellant can answer a question on their appeal', I => {
-  I.loginWithPIPAppellant();
-  I.answerQuestion('How do you interact with people?', 'By phone.');
-});
