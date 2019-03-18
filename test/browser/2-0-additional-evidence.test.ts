dc634351e282261d4e7614b9a93e83f24da9ca13
Toni Sanchez <toninos@gmail.com>
2019-03-18 12:18:46 +0000

    Adding unit and functional tests to evidence options page (#286)

---
+++ test/browser/2-0-additional-evidence.test.ts
@@ -0,0 +1,40 @@
+import { Page } from 'puppeteer';
+
+const { expect } = require('test/chai-sinon');
+import { startServices } from 'test/browser/common';
+import { TaskListPage } from 'test/page-objects/task-list';
+import { AdditionalEvidencePage } from 'test/page-objects/additional-evidence';
+import { allowedActions } from 'app/server/controllers/additional-evidence';
+const i18n = require('locale/en');
+
+describe('Additional Evidence', () => {
+  let page: Page;
+  let taskListPage: TaskListPage;
+  let additionalEvidencePage: AdditionalEvidencePage;
+  before('start services and bootstrap data in CCD/COH', async () => {
+    const res = await startServices({ performLogin: true });
+    page = res.page;
+    taskListPage = new TaskListPage(page);
+    additionalEvidencePage = new AdditionalEvidencePage(page);
+    // await taskListPage.visitPage();
+    await taskListPage.clickElement('#evidence-options-link');
+  });
+
+  after(async () => {
+    if (page && page.close) {
+      await page.close();
+    }
+  });
+
+  it('is on the evidence options page', () => {
+    additionalEvidencePage.verifyPage();
+  });
+
+  it('shows additional evidence options', async () => {
+    const header = await additionalEvidencePage.getElementText('h1');
+    expect(header).to.equal(i18n.additionalEvidence.evidenceOptions.header);
+
+    const options = await additionalEvidencePage.getElementsValues("input[name='additional-evidence-option']");
+    expect(options).to.have.members(allowedActions);
+  });
+});
---
+++ test/page-objects/additional-evidence.ts
@@ -0,0 +1,28 @@
+import { BasePage } from 'test/page-objects/base';
+import { additionalEvidence } from 'app/server/paths';
+
+export class AdditionalEvidencePage extends BasePage {
+  constructor(page) {
+    super(page);
+    this.pagePath = additionalEvidence;
+  }
+
+  async selectStatementOption() {
+    await this.clickElement('#additional-evidence-option-1');
+  }
+
+  async selectUploadOption() {
+    await this.clickElement('#additional-evidence-option-2');
+  }
+
+  async selectPostOption() {
+    await this.clickElement('#additional-evidence-option-3');
+  }
+
+  async submit() {
+    await Promise.all([
+      this.page.waitForNavigation(),
+      this.clickElement('#submit-button button')
+    ]);
+  }
+}
