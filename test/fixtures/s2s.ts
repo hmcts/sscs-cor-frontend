8e3c275715c81d724b22e0064d72b400bb60ba88
Izel Aciman <izelaciman@gmail.com>
2019-03-22 14:41:43 +0000

    Adding back oauth fixtures.

--- test/fixtures/s2s.ts
+++ test/fixtures/s2s.ts
@@ -1,9 +1,16 @@
 const config = require('config');
 const otp = require('otp');
 const rp = require('request-promise');
+
 const microservice = config.get('s2s.microservice');
 const s2sSecret = config.get('s2s.secret');
 const s2sUrl = config.get('s2s.url');
+const s2sOauthUrl = config.get('s2s.oauth2.url');
+const systemUpdateUser = config.get('s2s.oauth2.user');
+const systemUpdatePassword = config.get('s2s.oauth2.password');
+const clientSecret = config.get('s2s.oauth2.client.secret');
+const redirectUrl = config.get('s2s.oauth2.redirectUrl');
+
 const { Logger } = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('question.ts');
 const timeout = require('config').get('apiCallTimeout');
@@ -15,7 +22,7 @@ interface TokenResponse {
 interface AuthorizeResponse {
   code: string;
 }
-// todo turn this into an object that can be injected
+
 async function generateToken(): Promise<string> {
   const oneTimePassword = otp({ secret: s2sSecret }).totp();
   const options = {
@@ -40,6 +47,63 @@ async function generateToken(): Promise<string> {
   return body;
 }
 
+async function generateOauth2(): Promise<string> {
+  const authorizeToken: AuthorizeResponse = await authorize();
+  const tokenResponse: TokenResponse = await getToken(authorizeToken.code);
+  return tokenResponse.access_token;
+}
+
+async function authorize(): Promise<AuthorizeResponse> {
+
+  let body;
+  try {
+    body = await rp.post({
+      uri: `${s2sOauthUrl}/oauth2/authorize`,
+      json: true,
+      headers: {
+        'Accept': 'application/json'
+      },
+      auth: {
+        user: systemUpdateUser,
+        pass: systemUpdatePassword
+      },
+      form: {
+        response_type: 'code',
+        client_id: microservice,
+        redirect_uri: redirectUrl
+      },
+      timeout
+    });
+  } catch (error) {
+    logger.error('Error authorize', error);
+  }
+
+  return Promise.resolve(body);
+}
+
+async function getToken(code: string): Promise<TokenResponse> {
+  let body;
+  try {
+    body = await rp.post({
+      uri: `${s2sOauthUrl}/oauth2/token`,
+      json: true,
+      form: {
+        code,
+        grant_type: 'authorization_code',
+        redirect_uri: redirectUrl,
+        client_id: microservice,
+        client_secret: clientSecret
+      },
+      timeout
+    });
+  } catch (error) {
+    logger.error('Error getToken', error);
+  }
+
+  return Promise.resolve(body);
+}
+
 export {
-  generateToken
+  generateToken,
+  generateOauth2
 };
