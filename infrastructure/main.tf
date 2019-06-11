eacad34fea542cce7fe85f1893e03736587f0ac8
Toni Sanchez <toninos@gmail.com>
2019-06-11 11:25:38 +0100

    Forcing app settings update adding a new one setting (#332)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -69,6 +69,7 @@ module "sscs-cor-frontend" {
     S2S_SECRET                                     = "${data.azurerm_key_vault_secret.sscs-s2s-secret.value}"
     MYA_FEATURE_FLAG                               = "${var.mya_feature_flag}"
     ADDITIONAL_EVIDENCE_FEATURE_FLAG               = "${var.additional_evidence_feature_flag}"
+    FORCE_CHANGE                                   = "true"
   }
 }
 
