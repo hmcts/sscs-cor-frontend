175615032370872f7c46019cfb351789336c7572
Izel Aciman <izelaciman@gmail.com>
2019-09-24 14:47:53 +0100

    Limit CSRF to only defined paths.

--- app/server/middleware/csrf.ts
+++ app/server/middleware/csrf.ts
@@ -1,11 +1,29 @@
 const csurf = require('csurf');
+import * as Paths from '../paths';
 
 function csrfToken(req, res, next) {
-  next();
+  const csrfMiddleware = csurf({ cookie: false });
+  if (CheckIfPathDefined(req.path)) {
+    csrfMiddleware(req, res, next);
+  } else {
+    next();
+  }
 }
 
 function csrfTokenEmbed(req, res, next) {
+  if (CheckIfPathDefined(req.path)) {
+    res.locals.csrfToken = req.csrfToken();
+  }
   next();
+}
+
+function CheckIfPathDefined(current) {
+  for (const path in Paths) {
+    if (current.includes(Paths[path])) {
+      return true;
+    }
+  }
+  return false;
 }
 
 export {
