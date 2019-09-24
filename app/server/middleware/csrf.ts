de32cfcdd75c7083ba8586a25f790a8cf6a58913
Izel Aciman <izelaciman@gmail.com>
2019-09-24 13:58:31 +0100

    csrf with cookie.

--- app/server/middleware/csrf.ts
+++ app/server/middleware/csrf.ts
@@ -2,7 +2,7 @@ const csurf = require('csurf');
 const csrfBlackList = ['/', '/sign-in', '/sign-out', '/register', '/idam-stub/login', '/login', '/idam-stub/oauth2/token'];
 
 function csrfToken(req, res, next) {
-  const csrfMiddleware = csurf();
+  const csrfMiddleware = csurf({ cookie: true });
   if (!csrfBlackList.includes(req.path)) {
     csrfMiddleware(req, res, next);
   } else {
