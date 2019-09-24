26bfad57442b0ec5721955e38e4087ed206c35fc
Izel Aciman <izelaciman@gmail.com>
2019-09-24 11:49:18 +0100

    exclude login bits from csrf

--- app/server/middleware/csrf.ts
+++ app/server/middleware/csrf.ts
@@ -1,9 +1,9 @@
 const csurf = require('csurf');
-const csrfBlackList = ['/', '/sign-in', '/sign-out', '/register'];
+const csrfBlackList = ['/', '/sign-in', '/sign-out', '/register', '/idam-stub/login', '/login', '/idam-stub/oauth2/token'];
 
 function csrfToken(req, res, next) {
   const csrfMiddleware = csurf();
-  if (req.session.accessToken && !csrfBlackList.includes(req.path)) {
+  if (!csrfBlackList.includes(req.path)) {
     csrfMiddleware(req, res, next);
   } else {
     next();
@@ -11,11 +11,10 @@ function csrfToken(req, res, next) {
 }
 
 function csrfTokenEmbed(req, res, next) {
-  if (req.session.accessToken && !csrfBlackList.includes(req.path)) {
+  if (!csrfBlackList.includes(req.path)) {
     res.locals.csrfToken = req.csrfToken();
   }
   next();
-
 }
 
 export {
