b7b7f35a592b5894cb0665e5c941aa62fa73d42d
Izel Aciman <izelaciman@gmail.com>
2019-09-24 14:30:51 +0100

    try removing it.

--- app/server/middleware/csrf.ts
+++ app/server/middleware/csrf.ts
@@ -1,18 +1,10 @@
 const csurf = require('csurf');
 
 function csrfToken(req, res, next) {
-  const csrfMiddleware = csurf({ cookie: false });
-  if (req.session.idamEmail) {
-    csrfMiddleware(req, res, next);
-  } else {
-    next();
-  }
+  next();
 }
 
 function csrfTokenEmbed(req, res, next) {
-  if (req.session.idamEmail) {
-    res.locals.csrfToken = req.csrfToken();
-  }
   next();
 }
 
