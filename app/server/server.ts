a6e66b5b3260443de9a2ba72d7f2c93ff1ec0095
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-01-22 21:45:26 +0000

    adding secrets using flex volumes

--- app/server/server.ts
+++ app/server/server.ts
@@ -1,7 +1,12 @@
+const config = require('@hmcts/properties-volume').addTo(require('config'));
+const setupSecrets = require('services/setupSecrets');
+
+// Setup secrets before loading the app
+setupSecrets();
+
 const { Logger } = require('@hmcts/nodejs-logging');
 import * as session from 'express-session';
 import * as redis from 'connect-redis';
-import * as config from 'config';
 import { setup } from './app';
 import { createSession } from './middleware/session';
 
