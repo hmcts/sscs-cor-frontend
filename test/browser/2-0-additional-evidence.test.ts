baff53a8ac0820a2367da06fcd9bd8e71320d71e
david.crespo <david.crespo@hmcts.net>
2020-02-02 03:12:46 +0100

    execute all tests

--- test/browser/2-0-additional-evidence.test.ts
+++ test/browser/2-0-additional-evidence.test.ts
@@ -96,7 +96,7 @@ describe('Additional Evidence', () => {
     expect(await additionalEvidenceUploadPage.getElementText('div.govuk-error-summary')).contain(i18n.additionalEvidence.evidenceUpload.error.noFilesUploaded);
   });
 
-  it.only('uploads a file and shows file list', async () => {
+  it('uploads a file and shows file list', async () => {
     additionalEvidencePage.verifyPage();
     await additionalEvidencePage.selectUploadOption();
     await additionalEvidencePage.submit();
