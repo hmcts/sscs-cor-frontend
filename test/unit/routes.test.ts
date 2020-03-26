e1c855cba1ccf48b865be91863ed4d9546a2e28b
srochani <sunil.rochani@hotmail.com>
2020-03-26 11:37:29 +0000

    SSCS-7229: MYA - Migrate unsubscribe hosted functionality from TYA to MYA

--- test/unit/routes.test.ts
+++ test/unit/routes.test.ts
@@ -5,6 +5,9 @@ import { setup } from '../../app/server/app';
 import { expect } from 'test/chai-sinon';
 const i18n = require('locale/en.json');
 const { createSession } = require('../../app/server/middleware/session');
+const HttpStatus = require('http-status-codes');
+const tribunalApiUrl = require('config').get('tribunals.api-url');
+const nock = require('nock');
 import { CONST } from '../../app/constants';
 import moment = require('moment');
 
@@ -22,6 +25,30 @@ describe('Routes', () => {
     dysonSetupCoh();
     dysonSetupIdam();
     dysonSetupS2s();
+
+    // Mock GET /tokens/macToken
+    nock(tribunalApiUrl)
+    .persist()
+    .get('/tokens/NnwxNDg3MDY1ODI4fDExN3BsSDdrVDc=')
+    .reply(HttpStatus.OK, {
+      token: {
+        appealId: 'md005',
+        subscriptionId: 1,
+        benefitType: 'pip',
+        decryptedToken: '3|1487025828|147plJ7kQ7'
+      }
+    });
+
+    // Mock POST /appeals/id/subscriptions/id
+    nock(tribunalApiUrl)
+    .post('/appeals/md005/subscriptions/1', { subscription: { email: 'person@example.com' } })
+    .reply(HttpStatus.OK);
+
+    // Mock DELETE /appeals/id/subscriptions/id
+    nock(tribunalApiUrl)
+    .delete('/appeals/md005/subscriptions/1')
+    .reply(HttpStatus.OK);
+
   });
 
   describe('Non secured routes', () => {
@@ -46,6 +73,83 @@ describe('Routes', () => {
       request(app)
         .get('/readiness')
         .expect(200, done);
+    });
+  });
+
+  describe('/manage-email-notifications/mactoken', () => {
+    const url = '/manage-email-notifications/NnwxNDg3MDY1ODI4fDExN3BsSDdrVDc=';
+
+    it('should respond with a HTTP 200 when performing a GET', done => {
+      request(app)
+      .get(url)
+      .expect(HttpStatus.OK, done);
+    });
+
+    it('should respond with a HTTP 400 when POSTing an \'unknown\' type', done => {
+      request(app)
+      .post(url)
+      .send({ type: 'unknown' })
+      .expect(HttpStatus.BAD_REQUEST, done);
+    });
+  });
+
+  describe('/manage-email-notifications/mactoken/stop', () => {
+    it('should respond with a HTTP 200 when performing a GET', done => {
+      request(app)
+      .get('/manage-email-notifications/NnwxNDg3MDY1ODI4fDExN3BsSDdrVDc=/stop')
+      .expect(HttpStatus.OK, done);
+    });
+  });
+
+  describe('/manage-email-notifications/mactoken/stopconfirm', () => {
+    it('should respond with a HTTP 200 when performing a GET', done => {
+      request(app)
+      .get('/manage-email-notifications/NnwxNDg3MDY1ODI4fDExN3BsSDdrVDc=/stopconfirm')
+      .expect(HttpStatus.OK, done);
+    });
+  });
+
+  describe.skip('/manage-email-notifications/mactoken/change', () => {
+    const url = '/manage-email-notifications/NnwxNDg3MDY1ODI4fDExN3BsSDdrVDc=/change';
+
+    it('should respond with a HTTP 200 when POSTing both email addresses', done => {
+      request(app)
+      .post(url)
+      .send({
+        email: 'person@example.com',
+        confirmEmail: 'person@example.com'
+      })
+      .expect(HttpStatus.OK, done);
+    });
+
+    it('should respond with a HTTP 400 when POSTing empty strings', done => {
+      request(app)
+      .post(url)
+      .send({
+        email: '',
+        confirmEmail: ''
+      })
+      .expect(HttpStatus.BAD_REQUEST, done);
+    });
+
+    it('should respond with a HTTP 400 when POSTing non email addresses', done => {
+      request(app)
+      .post(url)
+      .send({
+        email: 'rubb@ish',
+        confirmEmail: 'rubb@ish'
+      })
+      .expect(HttpStatus.BAD_REQUEST, done);
+    });
+
+    it('should respond with a 400 when POSTing email address that do not match', done => {
+      request(app)
+      .post(url)
+      .send({
+        email: 'person@example.com',
+        confirmEmail: 'person@example.net'
+      })
+      .expect(HttpStatus.BAD_REQUEST, done);
     });
   });
 
