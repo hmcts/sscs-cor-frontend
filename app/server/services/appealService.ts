7cd32885752b605e64aa841d4fcd98632d6ad977
srochani <sunil.rochani@hotmail.com>
2020-04-06 10:00:26 +0100

    SSCS-7229: MYA - Migrate unsubscribe hosted functionality from TYA to MYA

--- app/server/services/appealService.ts
+++ app/server/services/appealService.ts
@@ -1,36 +1,9 @@
-const { get } = require('lodash');
 const apiUrl = require('config').get('tribunals.api-url');
 const HttpStatus = require('http-status-codes');
 const request = require('superagent');
 import { Logger } from '@hmcts/nodejs-logging';
 
 const logger = Logger.getLogger('AppealService.js');
-
-const getAppeal = (req, res, next) => {
-  if (!req.params.id) {
-    next(new Error(`Unable to make API call to ${apiUrl}/appeals/${req.params.id}`));
-    return;
-  }
-
-  request.get(`${apiUrl}/appeals/${req.params.id}`)
-    .then(result => {
-      const appeal = result.body.appeal;
-      appeal.evidenceReceived = false;
-      appeal.latestEvents = appeal.latestEvents || [];
-      appeal.historicalEvents = appeal.historicalEvents || [];
-      res.locals.appeal = appeal;
-      logger.info(`GET /appeals/${req.params.id} ${HttpStatus.OK} for caseId ${appeal.caseId}`);
-      next();
-    }).catch(error => {
-      if (error.status === HttpStatus.NOT_FOUND) {
-        const message = get(error, 'response.body.message');
-        const path = get(error, 'response.body.path');
-        error.message = `${message} ${path}`;
-      }
-      // appInsights.trackException(error);
-      next(error);
-    });
-};
 
 const changeEmailAddress = (req, res, next) => {
   const token = res.locals.token;
@@ -74,4 +47,4 @@ const stopReceivingEmails = (req, res, next) => {
     });
 };
 
-module.exports = { getAppeal, changeEmailAddress, stopReceivingEmails };
+module.exports = { changeEmailAddress, stopReceivingEmails };
---
+++ test/unit/services/appealService.test.ts
@@ -0,0 +1,106 @@
+import * as path from 'path';
+import * as fs from 'fs';
+const {
+  changeEmailAddress,
+  stopReceivingEmails
+} = require('../services/appealService');
+const { expect, sinon } = require('test/chai-sinon');
+const HttpStatus = require('http-status-codes');
+const apiURL = require('config').get('tribunals.api-url');
+const appInsights = require('app-insights');
+const nock = require('nock');
+
+describe('appealService.js', () => {
+  let req = null;
+  let res = null;
+  let next = null;
+  let notificationsUrl = null;
+
+  beforeEach(() => {
+    req = {
+      params: {},
+      body: { email: 'myemail@email.com' }
+    };
+
+    res = {
+      locals: {
+        appeal: {},
+        token: {
+          appealId: 'md002',
+          subscriptionId: 'qwerty123'
+        }
+      }
+    };
+
+    next = sinon.stub();
+    notificationsUrl = `/appeals/${res.locals.token.appealId}/subscriptions/${res.locals.token.subscriptionId}`;
+  });
+
+  describe('changeEmailAddress() - POST 200', () => {
+    it('should call next() passing zero arguments', () => {
+      nock(apiURL)
+        .post(notificationsUrl, { subscription: { email: req.body.email } })
+        .reply(HttpStatus.OK);
+
+      return changeEmailAddress(req, res, next)
+        .then(() => {
+          return expect(next).to.have.been.called;
+        });
+    });
+  });
+
+  describe('changeEmailAddress() - POST 500', () => {
+    beforeEach(() => {
+      sinon.spy(appInsights, 'trackException');
+    });
+    afterEach(() => {
+      appInsights.trackException.restore();
+    });
+    it('should call next() with the error', () => {
+      const error = { value: HttpStatus.INTERNAL_SERVER_ERROR, reason: 'server error' };
+      nock(apiURL)
+        .post(notificationsUrl, { subscription: { email: req.body.email } })
+        .replyWithError(error);
+
+      return changeEmailAddress(req, res, next)
+        .then(() => {
+          expect(next).to.have.been.calledWith(error);
+          expect(appInsights.trackException).to.have.been.calledOnce.calledWith(error);
+        });
+    });
+  });
+
+  describe('stopReceivingEmails() - DELETE 200', () => {
+    it('should call next() passing zero arguments', () => {
+      nock(apiURL)
+        .delete(notificationsUrl)
+        .reply(HttpStatus.OK);
+
+      return stopReceivingEmails(req, res, next)
+        .then(() => {
+          return expect(next).to.have.been.called;
+        });
+    });
+  });
+
+  describe('stopReceivingEmails() - DELETE 500', () => {
+    beforeEach(() => {
+      sinon.spy(appInsights, 'trackException');
+    });
+    afterEach(() => {
+      appInsights.trackException.restore();
+    });
+    it('should call next() passing an error containing a 500', () => {
+      const error = { value: HttpStatus.INTERNAL_SERVER_ERROR, reason: 'server error' };
+      nock(apiURL)
+        .delete(notificationsUrl)
+        .replyWithError(error);
+
+      return stopReceivingEmails(req, res, next)
+        .then(() => {
+          expect(next).to.have.been.calledWith(error);
+          expect(appInsights.trackException).to.have.been.calledOnce.calledWith(error);
+        });
+    });
+  });
+});
