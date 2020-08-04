47955bb43ea8666e54ea6d04361dac964c3c5cb0
LucaDelBuonoHMCTS <luca.delbuono@hmcts.com>
2020-08-04 14:28:29 +0100

    WLA-454: Amend Welsh content

--- app/server/setLanguage.ts
+++ app/server/setLanguage.ts
@@ -5,16 +5,12 @@ const languages = require('config').languages;
 const logger = Logger.getLogger('app-configuration.ts');
 
 router.get('*', (req, res, next) => {
-  logger.info(`Language currently set to ${i18next.language.toUpperCase()}`);
-
   if (!req.session.language) {
     req.session.language = 'en';
   } else if (req.query && req.query.lng && languages.includes(req.query.lng)) {
-    logger.info(`Setting language to ${req.query.lng.toUpperCase()} from query string`);
     req.session.language = req.query.lng;
     i18next.changeLanguage(req.query.lng);
   } else {
-    logger.info(`Setting language to ${req.session.language.toUpperCase()} from session`);
     i18next.changeLanguage(req.session.language);
   }
 
