a8ec16ebfce801220262d4b86cfe66c1f46f81b4
LucaDelBuonoHMCTS <luca.delbuono@hmcts.com>
2020-08-04 08:51:59 +0100

    WLA-454: Set language in session

--- app/server/routes.ts
+++ app/server/routes.ts
@@ -103,9 +103,16 @@ router.use((req, res, next) => {
 
 router.get('*', (req, res, next) => {
   logger.info(`Language currently set to ${i18next.language.toUpperCase()}`);
-  if (req.query && req.query.lng && languages.includes(req.query.lng)) {
-    logger.info(`Setting language to ${req.query.lng.toUpperCase()}`);
+
+  if (!req.session.language) {
+    req.session.language = 'en';
+  } else if (req.query && req.query.lng && languages.includes(req.query.lng)) {
+    logger.info(`Setting language to ${req.query.lng.toUpperCase()} from query string`);
+    req.session.language = req.query.lng;
     i18next.changeLanguage(req.query.lng);
+  } else {
+    logger.info(`Setting language to ${req.session.language.toUpperCase()} from session`);
+    i18next.changeLanguage(req.session.language);
   }
 
   next();
