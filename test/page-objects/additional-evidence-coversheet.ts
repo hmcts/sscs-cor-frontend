30cfa4eaed0e110afff9f10e6ab244763e100755
gokul-sol <gokul.sridharan@solirius.com>
2020-10-06 18:40:25 +0100

    Fix lint issues

--- test/page-objects/additional-evidence-coversheet.ts
+++ test/page-objects/additional-evidence-coversheet.ts
@@ -6,13 +6,12 @@ const config = require('config');
 const testUrl = config.get('testUrl');
 
 export class AdditionalEvidenceCoversheetPage extends BasePage {
+  constructor(page) {
+    super(page);
+    this.pagePath = coversheet;
+  }
 
-    constructor(page){
-        super(page);
-        this.pagePath = coversheet;
-    }
-
-    async navigateToCoverSheetPage(){
-        await this.page.goto(`${testUrl}${this.pagePath}`, {waitUntil : 'networkidle2' }).catch(e => void 0);
-    }
+  async navigateToCoverSheetPage() {
+    await this.page.goto(`${testUrl}${this.pagePath}`, { waitUntil : 'networkidle2' }).catch(e => void 0);
+  }
 }
