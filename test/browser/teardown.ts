44083ace193b96e6aa2eb362386b14ad3e7c31f5
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-03-16 14:48:48 +0000

    console log for testing

--- test/browser/teardown.ts
+++ test/browser/teardown.ts
@@ -2,6 +2,7 @@ import * as sidam from 'test/fixtures/sidam';
 
 export async function deleteIdamUser(user) {
   try {
+    console.log('user in teardown is..', user);
     return await sidam.deleteUser(user);
   } catch (error) {
     return Promise.reject(error);
