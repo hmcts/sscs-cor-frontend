fb74c55380019502cb95ba22fc5869445d7201ec
Jason.Hart <jason.hart@enginegroup.com>
2021-08-17 11:31:07 +0100

    SSCS-8611-exclude constants from sonar

--- test/unit/constants.test.ts
+++
@@ -1,8 +0,0 @@
-import { CONST } from '../../app/constants';
-const { expect } = require('test/chai-sinon');
-
-describe('/manage-email-notifications/mactoken', () => {
-  it('should contain 1 field', () => {
-    expect(CONST.HTTP_RETRIES).to.equal(3);
-  });
-});
