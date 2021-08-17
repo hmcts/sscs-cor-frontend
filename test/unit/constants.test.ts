a7f8a80d907cb4804a84e264f3520f60356b6d27
Jason.Hart <jason.hart@enginegroup.com>
2021-08-17 13:30:19 +0100

    SSCS-8611-exclude constants from coverage

--- test/unit/constants.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'test/chai-sinon';
-import * as CONST from 'app/constants';
-
-describe('constants.ts', () => {
-  it('should return 3 http retries', () => {
-    expect(CONST.CONST.HTTP_RETRIES).to.equal(3);
-  });
-});
