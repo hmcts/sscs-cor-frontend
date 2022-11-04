c7ae753a5ec24b06b3a72d326592094943eef8af
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-11-04 17:07:08 +0000

    SSCS-11066 Redis Update

--- app/server/middleware/session.ts
+++ app/server/middleware/session.ts
@@ -1,9 +1,29 @@
 import * as session from 'express-session';
-const config = require('config');
+import { Store } from 'express-session';
+import * as ConnectRedis from 'connect-redis';
+import { RedisStoreOptions } from 'connect-redis';
+import * as config from 'config';
+import IoRedis from 'ioredis';
+import { RequestHandler } from 'express';
 
-const isSecure = config.get('session.cookie.secure') === 'true';
+function createRedisStore(): Store {
+  const redisConnectionString: string = config.get('session.redis.url');
 
-function createSession(store?: session.Store) {
+  const client = new IoRedis(redisConnectionString);
+
+  const ttl: string = config.get('session.redis.ttlInSeconds');
+  const redisOpts: RedisStoreOptions = { client, ttl };
+
+  const ConnectRedisStore = ConnectRedis(session);
+
+  return new ConnectRedisStore(redisOpts);
+}
+
+function createSession(useRedisStore = false): RequestHandler {
+  const isSecure = config.get('session.cookie.secure') === 'true';
+
+  const store: Store = useRedisStore ? createRedisStore() : null;
+
   return session({
     cookie: {
       httpOnly: true,
