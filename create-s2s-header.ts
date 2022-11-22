2ff8813b46d6d1a585a06a4d55ff4f321f4a8b9b
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-11-22 15:16:39 +0000

    SSCS-11204 Fix create-s2s-header

--- create-s2s-header.ts
+++ create-s2s-header.ts
@@ -1,14 +1,28 @@
-import { bootstrapCreateS2sTokens } from './test/browser/bootstrap';
 import * as clc from 'cli-color';
+import { generateOauth2, generateToken } from './test/fixtures/s2s';
 
 const bold = clc.bold;
 const blue = clc.blueBright.bold;
 const white = clc.whiteBright;
 
+async function getHeaders(): Promise<{
+  Authorization: string;
+  ServiceAuthorization: string;
+  'Content-Type': string;
+}> {
+  const token = await generateToken();
+  const oauthToken: string = await generateOauth2();
+
+  return {
+    Authorization: `Bearer ${oauthToken}`,
+    ServiceAuthorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
+  };
+}
+
 /* tslint:disable no-console */
 async function runBootstrap() {
-  const { Authorization, ServiceAuthorization } =
-    await bootstrapCreateS2sTokens();
+  const { Authorization, ServiceAuthorization } = await getHeaders();
 
   console.log(`\n${blue(bold('Authorization: '))}${white(Authorization)}`);
   console.log(
