c3aa145b3b4711770c2806f7ee26dd32fd5c31e4
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-11-30 11:26:08 +0000

    Refining Error Handling

--- app/server/setLanguage.ts
+++ app/server/setLanguage.ts
@@ -1,20 +1,29 @@
-const router = require('express').Router();
-const { Logger } = require('@hmcts/nodejs-logging');
+import { Router } from 'express';
+import * as config from 'config';
+import { Logger } from '@hmcts/nodejs-logging';
+import { LoggerInstance } from 'winston';
+
 const i18next = require('i18next');
-const languages = require('config').languages;
 
-const logger = Logger.getLogger('app-configuration.ts');
+const i18n = require('i18next');
 
-router.get('*', (req, res, next) => {
-  if (!req.session.language) {
-    req.session.language = 'en';
+const router = Router();
+
+const languages: any = config.get('languages');
+
+const logger: LoggerInstance = Logger.getLogger('app-configuration.ts');
+
+router.get('*', async (req, res, next) => {
+  if (!req.session['language']) {
+    req.session['language'] = 'en';
   } else if (req.query?.lng && languages.includes(req.query.lng)) {
-    req.session.language = req.query.lng;
-    i18next.changeLanguage(req.query.lng);
+    req.session['language'] = req.query.lng;
+    await i18next.changeLanguage(req.query.lng as string);
   } else {
-    i18next.changeLanguage(req.session.language);
+    await i18next.changeLanguage(req.session['language']);
   }
 
+  logger.info(`Language is set to ${i18n.language}`);
   next();
 });
 
