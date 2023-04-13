5267baf8e252326463cf57f33fb1afafb1df4889
Mohammed Ali <mohammed.ali@hmcts.net>
2023-04-13 17:02:31 +0100

    Fix 13.04.23

--- app/server/app-configurations.ts
+++ app/server/app-configurations.ts
@@ -8,8 +8,6 @@ import helmet from 'helmet';
 import config from 'config';
 import { tyaNunjucks } from './controllers/content';
 import { dateFormat } from './utils/dateUtils';
-import { ContentSecurityPolicyOptions } from 'helmet/dist/types/middlewares/content-security-policy';
-import { ReferrerPolicyOptions } from 'helmet/dist/types/middlewares/referrer-policy';
 import content from '../common/locale/content.json';
 import * as path from 'path';
 
@@ -19,59 +17,55 @@ const defaultDateFormat: string = config.get('default.dateFormat');
 
 const DecisionReceivedDaysAfterHearing = 5;
 
-// Helmet referrer policy
-const referrerPolicy: ReferrerPolicyOptions = { policy: 'origin' };
-
-// Helmet content security policy (CSP) to allow only assets from same domain.
-const contentSecurityPolicy: ContentSecurityPolicyOptions = {
-  directives: {
-    defaultSrc: ["'self'"],
-    fontSrc: ["'self' data:"],
-    scriptSrc: [
-      "'self'",
-      "'unsafe-inline'",
-      'www.google-analytics.com',
-      'www.googletagmanager.com',
-      'tagmanager.google.com',
-      'vcc-eu4.8x8.com',
-      'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js',
-      'https://code.jquery.com/ui/1.12.1/jquery-ui.js',
-      'https://code.jquery.com/jquery-3.6.0.js',
-    ],
-    styleSrc: [
-      "'self'",
-      "'unsafe-inline'",
-      'tagmanager.google.com',
-      'fonts.googleapis.com/',
-    ],
-    connectSrc: [
-      "'self'",
-      'www.gov.uk',
-      '//localhost:9856/',
-      'www.google-analytics.com',
-      'www.googletagmanager.com',
-    ],
-    mediaSrc: ["'self'"],
-    frameSrc: ["'self'", 'www.googletagmanager.com', 'vcc-eu4.8x8.com'],
-    frameAncestors: ["'self'", 'www.googletagmanager.com'],
-    imgSrc: [
-      "'self'",
-      "'self' data:",
-      'www.google-analytics.com',
-      'www.googletagmanager.com',
-      'tagmanager.google.com',
-      'vcc-eu4.8x8.com',
-    ],
-  },
-};
-
 export function configureHelmet(app: Application): void {
   // by setting HTTP headers appropriately.
   app.use(helmet());
 
-  app.use(helmet.referrerPolicy(referrerPolicy));
+  app.use(helmet.referrerPolicy({ policy: 'origin' }));
 
-  app.use(helmet.contentSecurityPolicy(contentSecurityPolicy));
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        fontSrc: ["'self' data:"],
+        scriptSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          'www.google-analytics.com',
+          'www.googletagmanager.com',
+          'tagmanager.google.com',
+          'vcc-eu4.8x8.com',
+          'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js',
+          'https://code.jquery.com/ui/1.12.1/jquery-ui.js',
+          'https://code.jquery.com/jquery-3.6.0.js',
+        ],
+        styleSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          'tagmanager.google.com',
+          'fonts.googleapis.com/',
+        ],
+        connectSrc: [
+          "'self'",
+          'www.gov.uk',
+          '//localhost:9856/',
+          'www.google-analytics.com',
+          'www.googletagmanager.com',
+        ],
+        mediaSrc: ["'self'"],
+        frameSrc: ["'self'", 'www.googletagmanager.com', 'vcc-eu4.8x8.com'],
+        frameAncestors: ["'self'", 'www.googletagmanager.com'],
+        imgSrc: [
+          "'self'",
+          "'self' data:",
+          'www.google-analytics.com',
+          'www.googletagmanager.com',
+          'tagmanager.google.com',
+          'vcc-eu4.8x8.com',
+        ],
+      },
+    })
+  );
 }
 
 export function configureHeaders(app: Application): void {
