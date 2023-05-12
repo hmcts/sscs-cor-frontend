ee9e5b20f3a2b4fa409d26c9d5b7d23ba1760bf6
jarekPierchala <jaroslaw.pierchala@hmcts.net>
2023-05-12 13:21:15 +0100

    refactor. Redis check on readiness

--- app/server/middleware/health.ts
+++ app/server/middleware/health.ts
@@ -1,10 +1,8 @@
-import { OK } from 'http-status-codes';
 import * as AppInsights from '../app-insights';
 import { LoggerInstance } from 'winston';
 import { Logger } from '@hmcts/nodejs-logging';
 import { createRedisClient } from './redis';
 import config from 'config';
-import os from 'os';
 import healthCheck from '@hmcts/nodejs-healthcheck';
 import outputs from '@hmcts/nodejs-healthcheck/healthcheck/outputs';
 
@@ -17,51 +15,13 @@ client.on('error', (error) => {
   AppInsights.trackTrace(`Health check failed on redis: ${error}`);
 });
 
-const healthTimeout: number = config.get('health.timeout');
-const healthDeadline: number = config.get('health.deadline');
-
-const apiUrl: string = config.get('api.url');
-const apiHealthUrl = `${apiUrl}/health`;
-const apiReadinessUrl = `${apiHealthUrl}/readiness`;
-
-const healthOptions = (message) => {
-  return {
-    callback: (error, res) => {
-      // eslint-disable-line id-blacklist
-      if (error) {
-        AppInsights.trackTrace(
-          `health_check_error: ${message} and error: ${error}`
-        );
-      }
-      return !error && res.status === OK ? outputs.up() : outputs.down(error);
-    },
-    timeout: healthTimeout,
-    deadline: healthDeadline,
-  };
-};
-
 function getHealthConfigure() {
   return healthCheck.configure({
     checks: {
-      redis: healthCheck.raw(() =>
-        client
-          .ping()
-          .then((_) => healthCheck.status(_ === 'PONG'))
-          .catch((error) => {
-            AppInsights.trackTrace(`Health check failed on redis: ${error}`);
-            return outputs.down(error);
-          })
-      ),
-      'manage-your-appeal-api': healthCheck.web(
-        apiHealthUrl,
-        healthOptions('Health check failed on manage-your-appeal-api:')
-      ),
+      'manage-your-appeal-api': healthCheck.web(`${config.api.url}/health`)
     },
-    buildInfo: {
-      name: 'Manage Your Appeal',
-      host: os.hostname(),
-      uptime: process.uptime(),
-    },
+    timeout: config.health.timeout,
+    deadline: config.health.deadline
   });
 }
 
@@ -77,16 +37,8 @@ function getReadinessConfigure() {
             return outputs.down(error);
           })
       ),
-      'mmanage-your-appeal-api': healthCheck.web(
-        apiReadinessUrl,
-        healthOptions('Readiness check failed on manage-your-appeal-api:')
-      ),
-    },
-    buildInfo: {
-      name: 'Manage Your Appeal',
-      host: os.hostname(),
-      uptime: process.uptime(),
-    },
+      'mmanage-your-appeal-api': healthCheck.web(`${config.api.url}/health/readiness`)
+    }
   });
 }
 
