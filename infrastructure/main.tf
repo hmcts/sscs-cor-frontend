097fb2c1c488678eb66c5a06dfcf8917952d6c4e
paul-pearson <paul.pearson@version1.com>
2023-06-27 17:17:45 +0100

    Revert "Correct Redis URL and update version"
    This reverts commit f12be77a292cea1c65fb40fc65923039905a49b2.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,13 +18,13 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
 }
 
 module "redis-cache" {
-  source        = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product       = "${var.product}-redis"
-  location      = var.location
-  env           = var.env
-  redis_version = "6"
-  subnetid      = data.azurerm_subnet.core_infra_redis_subnet.id
-  common_tags   = var.common_tags
+  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product  = "${var.product}-redis"
+  location = var.location
+  env      = var.env
+
+  subnetid    = data.azurerm_subnet.core_infra_redis_subnet.id
+  common_tags = var.common_tags
 }
 
 resource "azurerm_key_vault_secret" "redis_access_key" {
@@ -40,7 +40,7 @@ resource "azurerm_key_vault_secret" "redis_access_key" {
 
 resource "azurerm_key_vault_secret" "redis_connection_string" {
   name         = "${var.product}-redis-connection-string"
-  value        = "rediss://:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}"
+  value        = "redis://ignore:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
 
   content_type = "secret"
