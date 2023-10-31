4441ba26e841e3d7a5d1089ca84f07ffb2e7c491
Gemma Talbot <gemma.talbot@solirius.com>
2023-10-31 10:45:16 +0000

    httpOnly and samSite flags set on connect.sid cookie

--- app/server/app.ts
+++ app/server/app.ts
@@ -87,7 +87,7 @@ export async function setupApp(
   app.use((req, res, next) => {
     const connect = req.cookies['connect.sid'];
 
-    res.cookie('connect.sid', connect, { secure: true, sameSite: 'strict' });
+    res.cookie('connect.sid', connect, { secure: true, httpOnly: true, sameSite: true });
     app.locals.webChat = config.get('services.webChat');
     app.locals.webFormUrl = config.get('services.webForm.url');
     app.locals.allowContactUs = isFeatureEnabled(
