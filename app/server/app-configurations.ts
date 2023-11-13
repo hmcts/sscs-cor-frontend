fc6b2d0639998435df7819018e0e6642c1e1950b
Gemma Talbot <gemma.talbot@solirius.com>
2023-11-13 15:47:46 +0000

    update to 3.7

--- app/server/app-configurations.ts
+++ app/server/app-configurations.ts
@@ -35,8 +35,7 @@ const contentSecurityPolicy: ContentSecurityPolicyOptions = {
       'tagmanager.google.com',
       'vcc-eu4.8x8.com',
       'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js',
-      'https://code.jquery.com/ui/1.12.1/jquery-ui.js',
-      'https://code.jquery.com/jquery-3.6.0.js',
+      'https://code.jquery.com/jquery-3.7.1.js',
     ],
     styleSrc: [
       "'self'",
