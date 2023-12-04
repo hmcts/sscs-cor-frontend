c51a5ee6a3dc81be7d05fecf7a4c74a9a07e607a
Gemma Talbot <gemma.talbot@solirius.com>
2023-12-04 15:18:27 +0000

    fix test

--- test/unit/controllers/login.test.ts
+++ test/unit/controllers/login.test.ts
@@ -299,7 +299,7 @@ describe('controllers/login', function () {
 
       it('logs AppInsights trace log', function () {
         expect(AppInsights.trackTrace).to.have.been.calledOnce.calledWith(
-          `[12345] - User logged in successfully as someEmail@example.com`
+          `[12345] - User logged in successfully as som...@ex...`
         );
       });
 
