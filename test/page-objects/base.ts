b0f15b30902eefdd23235723c894bc68972775a4
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2024-07-19 14:05:22 +0100

    SSCSSI-161: addedFT logging5

--- test/page-objects/base.ts
+++ test/page-objects/base.ts
@@ -44,8 +44,6 @@ export class BasePage {
   }
 
   verifyPage() {
-    console.log('this.page.url=', this.page.url());
-    console.log('testUrl.pagePath=', testUrl + this.pagePath);
     expect(this.page.url()).to.contain(`${testUrl}${this.pagePath}`);
   }
 
