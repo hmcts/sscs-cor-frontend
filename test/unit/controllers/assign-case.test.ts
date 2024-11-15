57e0c158ef482902d66ce252e347a93dcc74c600
taleb <tech@benouaer.com>
2024-11-15 16:31:59 +0000

    SSCSCI-1204: add ibca validation tests

--- test/unit/controllers/assign-case.test.ts
+++ test/unit/controllers/assign-case.test.ts
@@ -1,6 +1,6 @@
 import { getIndex, postIndex } from 'app/server/controllers/assign-case';
 import { expect, sinon } from '../../chai-sinon';
-import { OK } from 'http-status-codes';
+import { StatusCodes } from 'http-status-codes';
 import { CaseService } from 'app/server/services/cases';
 import { TrackYourApealService } from 'app/server/services/tyaService';
 import content from 'app/common/locale/content.json';
@@ -40,161 +40,304 @@ describe('controllers/assign-case.js', function () {
     let trackYourAppealService: TrackYourApealService;
     let underTest;
 
-    beforeEach(function () {
-      onlineHearing = {
-        hearingId: 'hearingId',
-        case_id: caseId,
-      };
-      appeal = {
-        hearingType: 'paper',
-      };
-
-      caseService = {
-        assignOnlineHearingsToCitizen: sinon.stub().resolves({
-          statusCode: OK,
-          body: onlineHearing,
-        }),
-      } as any;
-      trackYourAppealService = {
-        getAppeal: sinon.stub().resolves({
-          statusCode: OK,
-          appeal,
-        }),
-      } as any;
-    });
-
-    describe('for valid postcode', function () {
-      const postcode = 'cm11 1ab';
-
+    describe('post with valid data', function () {
       beforeEach(function () {
-        req = {
-          session: { idamEmail, tya },
-          body: { postcode },
-        } as any;
+        onlineHearing = {
+          hearingId: 'hearingId',
+          case_id: caseId,
+        };
+        appeal = {
+          hearingType: 'paper',
+        };
 
-        underTest = postIndex(caseService, trackYourAppealService);
-      });
-
-      it('assigns user to case', async function () {
-        await underTest(req, res);
-
-        expect(
-          caseService.assignOnlineHearingsToCitizen
-        ).to.have.been.calledOnce.calledWith(idamEmail, tya, postcode, req);
-      });
-
-      it('gets appeal', async function () {
-        await underTest(req, res);
-
-        expect(
-          trackYourAppealService.getAppeal
-        ).to.have.been.calledOnce.calledWith(caseId, req);
-      });
-
-      it('redirects to task-list', async function () {
-        await underTest(req, res);
-
-        expect(res.redirect).to.have.been.calledOnce.calledWith('/status');
-      });
-
-      it('sets hearing in session', async function () {
-        await underTest(req, res);
-
-        expect(req.session.case).to.be.eql(onlineHearing);
-      });
-
-      it('sets appeal in session', async function () {
-        await underTest(req, res);
-
-        expect(req.session.appeal).to.be.eql(appeal);
-      });
-    });
-
-    describe('for missing postcode and hideHearing true', function () {
-      const postcode = 'cm11 1ab';
-
-      beforeEach(function () {
-        trackYourAppealService = {
-          getAppeal: sinon.stub().resolves({
-            statusCode: OK,
-            appeal: {
-              hearingType: 'paper',
-              hideHearing: true,
-            },
+        caseService = {
+          assignOnlineHearingsToCitizen: sinon.stub().resolves({
+            statusCode: StatusCodes.OK,
+            body: onlineHearing,
           }),
         } as any;
-
-        req = {
-          session: { idamEmail, tya },
-          body: { postcode },
+        trackYourAppealService = {
+          getAppeal: sinon.stub().resolves({
+            statusCode: StatusCodes.OK,
+            appeal,
+          }),
         } as any;
+      });
 
-        underTest = postIndex(caseService, trackYourAppealService);
+      describe('with valid postcode', function () {
+        const appealType = 'otherBenefits';
+        const postcode = 'cm11 1ab';
+        let ibcaReference;
+
+        beforeEach(function () {
+          req = {
+            session: { idamEmail, tya },
+            body: { appealType, postcode },
+          } as any;
+
+          underTest = postIndex(caseService, trackYourAppealService);
+        });
+
+        it('assigns user to case', async function () {
+          await underTest(req, res);
+
+          expect(
+            caseService.assignOnlineHearingsToCitizen
+          ).to.have.been.calledOnce.calledWith(idamEmail, tya, postcode, ibcaReference, req);
+        });
+
+        it('gets appeal', async function () {
+          await underTest(req, res);
+
+          expect(
+            trackYourAppealService.getAppeal
+          ).to.have.been.calledOnce.calledWith(caseId, req);
+        });
+
+        it('redirects to task-list', async function () {
+          await underTest(req, res);
+
+          expect(res.redirect).to.have.been.calledOnce.calledWith('/status');
+        });
+
+        it('sets hearing in session', async function () {
+          await underTest(req, res);
+
+          expect(req.session.case).to.be.eql(onlineHearing);
+        });
+
+        it('sets appeal in session', async function () {
+          await underTest(req, res);
+
+          expect(req.session.appeal).to.be.eql(appeal);
+        });
+      });
+
+      describe('with valid ibcaReference', function () {
+        const appealType = 'ibca';
+        let postcode;
+        const ibcaReference = 'aa1bb2';
+
+        beforeEach(function () {
+          req = {
+            session: { idamEmail, tya },
+            body: { appealType, ibcaReference },
+          } as any;
+
+          underTest = postIndex(caseService, trackYourAppealService);
+        });
+
+        it('assigns user to case', async function () {
+          await underTest(req, res);
+
+          expect(
+            caseService.assignOnlineHearingsToCitizen
+          ).to.have.been.calledOnce.calledWith(idamEmail, tya, postcode, ibcaReference, req);
+        });
+
+        it('gets appeal', async function () {
+          await underTest(req, res);
+
+          expect(
+            trackYourAppealService.getAppeal
+          ).to.have.been.calledOnce.calledWith(caseId, req);
+        });
+
+        it('redirects to task-list', async function () {
+          await underTest(req, res);
+
+          expect(res.redirect).to.have.been.calledOnce.calledWith('/status');
+        });
+
+        it('sets hearing in session', async function () {
+          await underTest(req, res);
+
+          expect(req.session.case).to.be.eql(onlineHearing);
+        });
+
+        it('sets appeal in session', async function () {
+          await underTest(req, res);
+
+          expect(req.session.appeal).to.be.eql(appeal);
+        });
       });
     });
 
-    describe('for missing postcode', function () {
-      const postcode = '';
+    describe('post with missing data', function () {
 
       beforeEach(function () {
         req = {
           session: { idamEmail, tya },
-          body: { postcode },
         } as any;
 
         underTest = postIndex(caseService, trackYourAppealService);
       });
 
-      it('redirects to task-list', async function () {
+      it('missing appealType', async function () {
+        let appealType;
+        const postcode = '';
+        req.body = { appealType, postcode };
+        const error = {
+          msg: content.en.assignCase.errors.missing.appealType,
+          code: 'missing-appealType',
+        };
         await underTest(req, res);
 
-        expect(res.render).to.have.been.calledOnce.calledWith(
-          'assign-case/index.njk',
-          { error: content.en.assignCase.errors.noPostcode }
+        expect(res.render).to.have.been.calledOnce.calledWith('assign-case/index.njk',
+          { error, ...req.body }
+        );
+      });
+
+      it('missing postcode', async function () {
+        const appealType = 'otherBenefits';
+        const postcode = '';
+        req.body = { appealType, postcode };
+        const error = {
+          msg: content.en.assignCase.errors.missing.postcode,
+          code: 'missing-postcode',
+        };
+        await underTest(req, res);
+
+        expect(res.render).to.have.been.calledOnce.calledWith('assign-case/index.njk',
+          { error, ...req.body }
+        );
+      });
+
+      it('missing ibcaReference', async function () {
+        const appealType = 'ibca';
+        const postcode = '';
+        req.body = { appealType, postcode };
+        const error = {
+          msg: content.en.assignCase.errors.missing.ibcaReference,
+          code: 'missing-ibcaReference',
+        };
+        await underTest(req, res);
+
+        expect(res.render).to.have.been.calledOnce.calledWith('assign-case/index.njk',
+          { error, ...req.body }
         );
       });
     });
 
-    describe('for invalid postcode', function () {
-      const postcode = 'invalid';
+    describe('post with invalid data', function () {
 
       beforeEach(function () {
         req = {
           session: { idamEmail, tya },
-          body: { postcode },
         } as any;
 
         underTest = postIndex(caseService, trackYourAppealService);
       });
 
-      it('redirects to task-list', async function () {
+      it('invalid postcode', async function () {
+        const appealType = 'otherBenefits';
+        const postcode = 'invalid';
+        req.body = { appealType, postcode };
+        const error = {
+          msg: content.en.assignCase.errors.invalid.postcode,
+          code: 'invalid-postcode',
+        };
+
         await underTest(req, res);
 
         expect(res.render).to.have.been.calledOnce.calledWith(
           'assign-case/index.njk',
-          { error: content.en.assignCase.errors.invalidPostcode }
+          { error, ...req.body }
+        );
+      });
+
+      it('invalid ibcaReference', async function () {
+        const appealType = 'ibca';
+        const ibcaReference = 'invalid';
+        req.body = { appealType, ibcaReference };
+        const error = {
+          msg: content.en.assignCase.errors.invalid.ibcaReference,
+          code: 'invalid-ibcaReference',
+        };
+
+        await underTest(req, res);
+
+        expect(res.render).to.have.been.calledOnce.calledWith(
+          'assign-case/index.njk',
+          { error, ...req.body }
         );
       });
     });
 
-    describe('for missing tya', function () {
-      const postcode = 'TS1 1ST';
+    describe('post with missing tya', function () {
 
       beforeEach(function () {
         req = {
           session: { idamEmail },
-          body: { postcode },
         } as any;
 
         underTest = postIndex(caseService, trackYourAppealService);
       });
 
       it('redirects to task-list', async function () {
+        const appealType = 'otherBenefits';
+        const postcode = 'TS1 1ST';
+        req.body = { appealType, postcode };
+        const error = {
+          msg: content.en.assignCase.errors.missing.tya,
+          code: 'tyaNotProvided',
+        };
+
         await underTest(req, res);
 
         expect(res.render).to.have.been.calledOnce.calledWith(
           'assign-case/index.njk',
-          { error: content.en.assignCase.errors.tyaNotProvided }
+          { error, ...req.body }
+        );
+      });
+    });
+
+    describe('no matching data', function () {
+      beforeEach(function () {
+        caseService = {
+          assignOnlineHearingsToCitizen: sinon.stub().resolves({
+            statusCode: StatusCodes.BAD_REQUEST,
+            body: {},
+          }),
+        } as any;
+        req = {
+          session: { idamEmail, tya },
+        } as any;
+
+        underTest = postIndex(caseService, trackYourAppealService);
+      });
+
+      it('no matching postcode', async function () {
+        const appealType = 'otherBenefits';
+        const postcode = 'TS1 1ST';
+        req.body = { appealType, postcode };
+        const error = {
+          msg: content.en.assignCase.errors.invalid.postcode,
+          code: 'no-matching-record',
+        };
+
+        await underTest(req, res);
+
+        expect(res.render).to.have.been.calledOnce.calledWith(
+          'assign-case/index.njk',
+          { error, ...req.body }
+        );
+      });
+
+      it('no matching ibcaReference', async function () {
+        const appealType = 'ibca';
+        const ibcaReference = 'TS1ST1';
+        req.body = { appealType, ibcaReference };
+        const error = {
+          msg: content.en.assignCase.errors.invalid.ibcaReference,
+          code: 'no-matching-record',
+        };
+
+        await underTest(req, res);
+
+        expect(res.render).to.have.been.calledOnce.calledWith(
+          'assign-case/index.njk',
+          { error, ...req.body }
         );
       });
     });
