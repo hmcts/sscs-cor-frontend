6add3e48dc9ceecffda5e1ec9e5a7c87322966d9
Pettedson John <pettedson.john1@hmcts.net>
2025-01-07 16:34:01 +0000

    test: fix for the post code verification issue for benefit appeals.

--- test/crossbrowser/pages/postcode/verifyAppeallantPostCode.js
+++ test/crossbrowser/pages/postcode/verifyAppeallantPostCode.js
@@ -1,6 +1,8 @@
 function verifyAppeallantPostCode() {
   const I = this;
   I.wait(5);
+  I.click('[for=\'appeal-type-otherBenefits\']')
+  I.wait(1);
   I.fillField('#postcode', 'TN32 6PL');
   I.click('#assign-case');
 }
@@ -11,6 +13,8 @@ function verifyWelshAppellantPostCode() {
   I.wait(5);
   I.click('.govuk-link.language');
   I.wait(5);
+  I.click('[for=\'appeal-type-otherBenefits\']')
+  I.wait(1);
   I.fillField('#postcode', 'TN32 6PL');
   I.click('#assign-case');
 }
