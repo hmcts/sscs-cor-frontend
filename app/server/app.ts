2502f563f38ed5433ba7bf1329098f3399645a15
Joshua Gray <joshgray91@gmail.com>
2025-05-02 18:18:47 +0100

    Update IBC appeals helpline (Scotland) to include Northern Ireland for English text

--- app/server/app.ts
+++ app/server/app.ts
@@ -107,6 +107,10 @@ export async function setupApp(
       Feature.CONTACT_US_WEBCHAT_ENABLED,
       req.cookies
     );
+    app.locals.ContactDetailsNIEnabled = isFeatureEnabled(
+      Feature.NI_CONTACT_DETAILS_ENABLED,
+      req.cookies
+    );
     app.locals.baseUrl = `${req.protocol}://${req.headers.host}`;
     next();
   });
