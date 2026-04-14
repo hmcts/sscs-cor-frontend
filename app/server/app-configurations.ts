6335406f9e92c4e73013d6a73ab7e8df18c2c1d6
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2026-04-14 16:16:04 +0100

    permission-policy update

--- app/server/app-configurations.ts
+++ app/server/app-configurations.ts
@@ -72,6 +72,7 @@ export function configureHeaders(app: Application): void {
   app.use((req, res, next) => {
     // Setting headers stops pages being indexed even if indexed pages link to them
     res.setHeader('X-Robots-Tag', 'noindex');
+    res.setHeader('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
     next();
   });
 }
