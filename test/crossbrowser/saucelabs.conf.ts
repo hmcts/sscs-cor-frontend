fc48ab823c6a797201754b21ef8401381a52034c
gokul-sol <gokul.sridharan@HMCTS.NET>
2026-09-08 16:19:09 +0100

    fix lint issues

--- test/crossbrowser/saucelabs.conf.ts
+++ test/crossbrowser/saucelabs.conf.ts
@@ -22,8 +22,8 @@ let reportDir = './functional-report'; // default for local runs
 //   reportDir = config.get('saucelabs.outputDir');
 // } else {
 //   // Jenkins expects these locations by the pipeline
-  codeceptOutput = 'test/crossbrowser/crossbrowser-output';
-  reportDir = 'test/crossbrowser/crossbrowser-report';
+codeceptOutput = 'test/crossbrowser/crossbrowser-output';
+reportDir = 'test/crossbrowser/crossbrowser-report';
 // }
 
 const helpers = {
