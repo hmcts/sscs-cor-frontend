cc14a4d271fefd07a50fed7552fc55426501b379
Gemma Talbot <gemma.talbot@solirius.com>
2026-09-16 12:52:50 +0100

    revet health check

--- app/server/middleware/health.ts
+++ app/server/middleware/health.ts
@@ -22,8 +22,6 @@ const healthDeadline: number = config.get('health.deadline');
 
 const apiUrl: string = config.get('tribunals-api.url');
 const apiHealthUrl = `${apiUrl}/health`;
-const hmctsAccessHealthBaseUrl: string =
-  config.get('health.idam.url.hmctsAccess') || config.get('idam.hmctsAccess');
 const apiReadinessUrl = `${apiHealthUrl}/readiness`;
 
 const healthOptions = (message) => {
@@ -92,33 +90,4 @@ function getReadinessConfigure() {
   });
 }
 
-function getHmctsAccessConfigure() {
-  return healthCheck.configure({
-    readinessChecks: {
-      redis: healthCheck.raw(() =>
-        client
-          .ping()
-          .then((_) => healthCheck.status(_ === 'PONG'))
-          .catch((error) => {
-            AppInsights.trackTrace(
-              `Hmcts Access health check failed on redis: ${error}`
-            );
-            return outputs.down(error);
-          })
-      ),
-      'manage-your-appeal-api': healthCheck.web(
-        hmctsAccessHealthBaseUrl,
-        healthOptions(
-          'Hmcts Access health check failed on manage-your-appeal-api:'
-        )
-      ),
-    },
-    buildInfo: {
-      name: 'Manage Your Appeal',
-      host: os.hostname(),
-      uptime: process.uptime(),
-    },
-  });
-}
-
-export { getHealthConfigure, getReadinessConfigure, getHmctsAccessConfigure };
+export { getHealthConfigure, getReadinessConfigure };
